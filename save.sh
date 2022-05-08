738f7215c8430521526ee287a5ee80deac0d51ab
Xavier R <295150+lougaou@users.noreply.github.com>
2022-05-08 19:42:15 +0200

    feat: add dev pwd hidden

--- save.sh
+++ save.sh
@@ -5,6 +5,8 @@ cd "$(dirname "${BASH_SOURCE}")"
 function saveFiles() {
 	rsync -avh --no-perms ~/.bashrc .
 	sed -i "/GITHUB_TOKEN=/c\export GITHUB_TOKEN=SECRET" .bashrc
+	sed -i "/DATABASE_PASSWORD=/c\export DATABASE_PASSWORD=SECRET" .bashrc
+	sed -i "/REDIS_PASSWORD=/c\export REDIS_PASSWORD=SECRET" .bashrc
 
 	rsync -avh --no-perms ~/.bash_alias .
 	rsync -avh --no-perms ~/.bash_functions .
